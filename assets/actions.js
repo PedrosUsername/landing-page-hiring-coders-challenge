5198ef1bec16338eb59e16be92edb27685593a27
rogs <pilique11@gmail.com>
2021-07-19 12:00:44 -0300

    complemento

--- assets/actions.js
+++ assets/actions.js
@@ -3,8 +3,10 @@ let cadastrar_btn = document.querySelector(".submit-btn");
 let name = document.getElementById("nome");
 let email = document.getElementById("email");
 
-let people = []
-let counter = 0;
+var counter = 0;
+let people = getPeople();
+
+
 
 
 cadastrar_btn.addEventListener("click", function(){
@@ -51,3 +53,27 @@ right_btn.addEventListener("click", function(){
     cards[carouselPos].classList.add("card-focus1");
     cards[carouselPos + 1].classList.add("card-focus2"); 
 });
+
+
+
+
+
+
+
+
+
+
+/* others */
+function getPeople() {
+    let arr = [];
+
+    for(let key in localStorage) {
+        if (!localStorage.hasOwnProperty(key)) {
+          continue; // skip keys like "setItem", "getItem" etc
+        }
+        arr.push({name: key, email: localStorage.getItem(key)});
+        counter++;
+      }
+
+    return arr;
+}
